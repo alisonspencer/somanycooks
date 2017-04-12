f7cc093faac1f61e73c90637022eca6bf71388ff
Alison <katelie42@gmail.com>
2017-04-12 19:42:51 -0400

    altered back to work with postgres db

--- views.py
+++ views.py
@@ -1,38 +1,38 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
+from flask import render_template, request
+from somanycooks import app
 import pandas as pd
+import psycopg2
+
 from Models import GetComments, CommentChecker
+from UserInfo import user, host, dbname
 
 
-#sentence_data = pd.read_csv("./static/data/comments_sentences.csv")
-recipe_data = pd.read_csv("./static/data/recipes_with_sentiment.csv")
+con = None
+con = psycopg2.connect(database = dbname, user=user,host=host)
 
 
-# recipe index - pandas/csv
+
+# recipe index
 recipe_table = []
-for i in range(0,recipe_data.shape[0]):
-    recipe_table.append(dict(recipe_title=recipe_data.iloc[i]['title'].decode('utf-8'), url=recipe_data.iloc[i]['url'].decode('utf-8')))
+query = """
+          SELECT title, url, rank_compound FROM recipes;
+          """
+query_results = pd.read_sql_query(query,con)
+for i in range(0,query_results.shape[0]):
+    recipe_table.append(dict(recipe_title=query_results.iloc[i]['title'].decode('utf-8'), url=query_results.iloc[i]['url'].decode('utf-8')))
 recipe_table = sorted(recipe_table)
 
 recipe_titles = []
-for i in range(0,recipe_data.shape[0]):
-    recipe_titles += [recipe_data.iloc[i]['title'].decode('utf-8')]
+for i in range(0,query_results.shape[0]):
+    recipe_titles += [query_results.iloc[i]['title'].decode('utf-8')]
 recipe_titles = sorted(recipe_titles)
 
 
-
-
 @app.route('/')
-def check():
-    return render_template('about.html')
-    #return "<h1 style='color:blue'>Hello There!</h1>"
-
 @app.route('/index')
 def main():
   return render_template('main.html', autocomplete_list=recipe_titles)
- # return render_template('main.html', autocomplete_list=['a','b','c','d','e','f'])
+
 
 # list all the recipes currently in the database
 @app.route('/recipe_index')
@@ -58,7 +58,7 @@ def find_comments():
     return render_template('recipe_index.html', recipes=recipe_table)
 
   elif user_input != '':
-    comments, url, rank, total_helpful, fraction_helpful, total_comments = GetComments(user_input, sentence_data, recipe_data)
+    comments, url, rank, total_helpful, fraction_helpful, total_comments = GetComments(user_input,con)
     happiness_graph = "../static/img/images/" + user_input + ".png"
     return render_template('recipe_data.html', commentslist=comments, title=user_input, url=url, image=happiness_graph, rank=rank,
       totalhelpful=total_helpful, fractionhelpful=fraction_helpful, numberofcomments=total_comments, numberofrecipes=len(recipe_titles))
@@ -70,7 +70,9 @@ def find_comments():
 @app.route('/check', methods=['GET', 'POST'])
 def check_comment():
   user_input = request.args.get('comment_check')
-  prediction, response = CommentChecker(user_input)
+  prediction, response = CommentChecker(user_input, con)
   return render_template('comment_checker.html', prediction=prediction, response=response, checkedcomment=user_input)
+
+
 
 
